db7b3ca5a27927111a28616b1143fe9da9fa185e
bee <404846146@qq.com>
2021-02-04 16:03:23 +0800

    fix bug

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,31 +1,31 @@
 <template>
   <div id="map-container"></div>
-  <div class="input-card">
-    <h4>聚合点效果切换</h4>
-    <div class="input-item">
-      <input
-        type="button"
-        class="btn"
-        value="默认样式"
-        id="add0"
-        onclick="addCluster(0)"
-      />
-      <input
-        type="button"
-        class="btn"
-        value="自定义图标"
-        id="add1"
-        onclick="addCluster(1)"
-      />
-      <input
-        type="button"
-        class="btn"
-        value="完全自定义"
-        id="add2"
-        onclick="addCluster(2)"
-      />
-    </div>
-  </div>
+  <!--  <div class="input-card">-->
+  <!--    <h4>聚合点效果切换</h4>-->
+  <!--    <div class="input-item">-->
+  <!--      <input-->
+  <!--        type="button"-->
+  <!--        class="btn"-->
+  <!--        value="默认样式"-->
+  <!--        id="add0"-->
+  <!--        onclick="addCluster(0)"-->
+  <!--      />-->
+  <!--      <input-->
+  <!--        type="button"-->
+  <!--        class="btn"-->
+  <!--        value="自定义图标"-->
+  <!--        id="add1"-->
+  <!--        onclick="addCluster(1)"-->
+  <!--      />-->
+  <!--      <input-->
+  <!--        type="button"-->
+  <!--        class="btn"-->
+  <!--        value="完全自定义"-->
+  <!--        id="add2"-->
+  <!--        onclick="addCluster(2)"-->
+  <!--      />-->
+  <!--    </div>-->
+  <!--  </div>-->
 </template>
 
 <script lang="ts">
@@ -35,32 +35,33 @@ import { points } from '@/mock/points.ts'
 export default defineComponent({
   name: 'Map',
   setup() {
-    let map
+    let map: {
+      addControl: (arg0: any) => void
+      add: (arg0: any) => void
+      getCenter: () => any
+    }
     let aMap
     onMounted(() => {
-      // 加载高德地图
+      // 加载高德地图 533351475b190d7df9bfc6a9c2ea89f2
       AMapLoader.load({
-        key: '533351475b190d7df9bfc6a9c2ea89f2', // 申请好的Web端开发者Key，首次调用 load 时必填
-        version: '2.0', // 指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15
-        plugins: ['AMap.MarkerCluster'], // 需要使用的的插件列表，如比例尺'AMap.Scale'等
-        AMapUI: {
-          // 是否加载 AMapUI，缺省不加载
-          version: '1.1', // AMapUI 缺省 1.1
-          plugins: [] // 需要加载的 AMapUI ui插件
-        }
-        // Loca: {
-        //   // 是否加载 Loca， 缺省不加载
-        //   version: '1.3.2' // Loca 版本，缺省 1.3.2
-        // }
+        //首次调用 load
+        key: '533351475b190d7df9bfc6a9c2ea89f2', //首次load key为必填
+        version: '2.0',
+        plugins: ['AMap.Scale', 'AMap.ToolBar', 'AMap.MarkerCluster']
       })
         .then(AMap => {
-          aMap = AMap
           map = new AMap.Map('map-container')
-          const gridSize = 60
-          AMap.MarkerCluster(map, points, { gridSize: gridSize })
+          map.addControl(new AMap.Scale())
+          map.addControl(new AMap.ToolBar())
+          map.add(
+            new AMap.Marker({
+              position: map.getCenter()
+            })
+          )
+          new AMap.MarkerCluster(map, points, { gridSize: 60 })
         })
         .catch(e => {
-          console.log(e)
+          console.error(e)
         })
     })
   }
